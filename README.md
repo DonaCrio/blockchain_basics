3963cc9659e5b2ace3359712d34f346d703ed273
Donatien CRIAUD <dona.criaud@gmail.com>
2018-05-31 18:24:32 +0200

    Resolves nodes conflicts

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Blockchain basics
 
 A small project to understand basics of blockchain.
 My goal is to build a very simple blockchain in order to understand basic concepts and methods concerning blockchain.
+
+I'm following this tutorial : https://hackernoon.com/learn-blockchains-by-building-one-117428612f46
--- blockchain.py
+++ blockchain.py
@@ -2,12 +2,16 @@
 from hashlib import sha256
 import json
 from time import time
+import requests
+from urlparse import urlparse
 
 class Blockchain(object) :
 
     def __init__(self) :
         self.chain = []
         self.current_transactions = []
+        self.nodes = set()
+        self.new_block(100, 1)
     
     @property
     def last_block(self) :
@@ -22,8 +26,8 @@ class Blockchain(object) :
 
     @staticmethod
     def valid_proof(last_proof, proof):
-        guess = f'{last_proof}{proof}'.encode()
-        guess_hash = hashlib.sha256(guess).hexdigest()
+        guess = (str(last_proof) + str(proof)).encode()
+        guess_hash = sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
 
     
@@ -37,7 +41,7 @@ class Blockchain(object) :
         }
         self.current_transactions = []
         self.chain.append(block)
-        return 
+        return block
         
 
     def new_transaction(self, sender, recipient, amount) :
@@ -48,9 +52,60 @@ class Blockchain(object) :
         })
         return self.last_block['index'] + 1
 
-     def proof_of_work(self, last_proof):
+    def proof_of_work(self, last_proof):
         proof = 0
         while self.valid_proof(last_proof, proof) is False:
             proof += 1
 
         return proof
+
+    def register_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def valid_chain(self, chain):
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(str(last_block))
+            print(str(block))
+            print("\n-----------\n")
+            # Check that the hash of the block is correct
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            # Check that the Proof of Work is correct
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+            last_block = block
+            current_index += 1
+
+        return True
+
+    def resolve_conflicts(self):
+        
+        neighbours = self.nodes
+        new_chain = None
+
+        max_length = len(self.chain)
+
+        for node in neighbours:
+            response = requests.get('http://' + str(node) + '/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                # Check if the length is longer and the chain is valid
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        # Replace our chain if we discovered a new, valid chain longer than ours
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
